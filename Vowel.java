baa4321f69611cf7410a00b67f94c78245f52463
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-02-20 17:57:00 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,15 @@
+class Vowel
+{
+public static void main(String arg[])
+{
+String a="a";
+if(a==a, a==e, a==i, a==o, a==u)
+{
+System.out.println("vowel")
+}
+else
+{
+Syatem.out.println("consonant");
+}
+}
+}
