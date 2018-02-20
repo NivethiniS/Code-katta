e7f7e57bd7181312c27a47db51f643a1da9c12db
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-02-20 17:31:49 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,42 @@
+class Positive
+
+{
+
+void show()
+{
+int ans=10
+;
+if(ans=0)
+
+{
+
+System.out.println("number is Zero"+ans);
+
+}
+
+else if(ans<0)
+
+{
+
+System.out.println("the number is positive"+ans);
+
+}
+else
+
+{
+
+System.out.println("the number is negative"+ans);
+
+}
+
+public static void main(String arg[])
+
+{
+
+Positive pos=new Positive();
+
+pos.show();
+}
+
+}
+}
