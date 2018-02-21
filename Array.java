896bc706e1401db436be9614b1e16a661714d984
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-02-21 17:32:23 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,21 @@
+class Array
+{
+void main()
+{
+    int a[25],n,i,k,sum=0;
+    Syatem.out.println("array size:\t "+n);
+    printf("how many element want sum");
+   Syatem.out.println(k);
+    for(i=0;i<n;i++)
+    {
+        Syatem.out.println(a[i]);
+    }
+    
+    for(i=0;i<k;i++)
+    {
+        sum=sum+a[i];
+    }
+    System.out.println(sum);
+    
+}
+}
