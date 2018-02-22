59880291626156fd112896a1288c86bf61728cdd
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-02-22 17:46:14 +0530

    Create EvenNum.java

---
+++ EvenNum.java
@@ -0,0 +1,22 @@
+class EvenNum
+{
+Public static void main(String a[])
+{
+ int a,b;
+
+System.out.println(a+b);
+
+ for(int i=a+1;i<b;i++)
+
+ {
+
+ 	if((i%2)==0)
+
+System.out.println(i);
+
+ }
+
+	return 0;
+
+}
+}
