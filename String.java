49fccc88fa937df75d1c651c878fa28869f02d73
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-02-28 17:10:57 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,17 @@
+class String
+{
+String name="Laptop";
+int n=5;
+void display()
+{
+for(i=0; i<n; i++)
+{
+System.out.println(name);
+}
+}
+public static void main(String arg[])
+{
+String str=new String();
+str.display();
+}
+}
