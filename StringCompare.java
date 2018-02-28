5528c32fc8a26c59bab5871daf464486987b2501
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-02-28 17:29:15 +0530

    Create StringCompare.java

---
+++ StringCompare.java
@@ -0,0 +1,34 @@
+class StringCompare
+
+{
+
+public static void main(String arg[])
+
+{
+
+String name1="Nivethini";
+
+String name2="Guvi";
+
+if(name1.length()==name2.length())
+
+{
+System.out.println(name1);
+
+}
+
+else if(name1.length()<name2.length())
+
+{
+System.out.println(name1);
+
+}
+
+else
+
+{
+
+System.out.println(name2);
+}
+}
+}
