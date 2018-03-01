7bf4d8d02c84fb608d28ed4657e811aeee42990c
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-03-01 17:16:48 +0530

    Update NextNumber.java

--- NextNumber.java
+++ NextNumber.java
@@ -1,11 +1,17 @@
 class NextNumber
 {
+
 public static void main(String arg[])
 {
+
 int a=10;
-for(i=0; i<a; i++)
+
+for(int i=0; i<1; i++)
+
 {
+
 a++;
+
 System.out.println("the next num is "+a);
 }
 }
