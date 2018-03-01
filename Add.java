a25cac0129f7eed0453747ffc6fc50999e7bb53c
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-03-01 17:53:00 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Add
+
+{
+
+public static void main(String arg[])
+
+{
+
+
+int a;
+int b;
+int ans;
+System.out.println("Enter a ");
+
+
+Scanner sc=new Scanner(System.in);
+
+a=sc.nextInt();
+
+System.out.println(a);
+
+b=sc.nextInt();
+
+System.out.println("Enter b "+b);
+
+
+ans=a+b;
+
+
+System.out.println("Total="+ans);
+}
+}
