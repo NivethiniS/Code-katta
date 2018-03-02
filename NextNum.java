8bcfc7f5aba3d3ecab5befbeda10e3e7cfbe3574
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-03-02 17:29:23 +0530

    Create NextNum.java

---
+++ NextNum.java
@@ -0,0 +1,19 @@
+class NextNum
+ {
+public static void main(String arg[])
+{
+	int num=10,even;
+	
+	System.out.println(num);
+	if(num%2==0)
+	{
+		even=num-2;	
+	}
+	else
+	{
+		even=num-1;
+	}
+System.out.println(even);
+	return 0;
+}
+}
