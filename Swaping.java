2a3cfd5b06e104860501ae5dc5257ead7e46f39c
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-03-03 17:38:53 +0530

    Create Swaping.java

---
+++ Swaping.java
@@ -0,0 +1,14 @@
+class Swaping
+{
+int a=37;
+int b=34;
+System.out.println("Before swapping");
+System.out.println("a="+a);
+System.out.println("b="+b);
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println("after swapping");
+System.out.println("a="+a);
+System.out.println("b="+b);
+}
