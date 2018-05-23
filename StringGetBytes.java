5eb3c6eab298ffb08d3e66bd113d14b3044bcfe4
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-05-23 10:35:57 +0530

    Update and rename StringGetBytesExample.java to StringGetBytes.java

--- StringGetBytes.java
+++ StringGetBytes.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
-public class StringGetBytesExample{  
+public class StringGetBytes{  
+    
 public static void main(String args[]){  
     Scanner scan=new Scanner(System.in);
 String s1=scan.nextLine();
 System.out.println(s1);
+    
 byte[] barr=s1.getBytes();  
 for(int i=0;i<barr.length;i++){  
 System.out.println(barr[i]);  
