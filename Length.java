a50bbc9bf7e564ef7035c23a3cef6edd0858cde1
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-05-23 11:46:34 +0530

    Create Length.java

---
+++ Length.java
@@ -0,0 +1,18 @@
+
+
+import java.util.*;
+import java.lang.*;
+
+import java.util.Scanner;
+public class Length
+{
+     public static void main(String []args){
+          Scanner sc=new Scanner(System.in);  
+         String name=sc.nextLine();
+         System.out.println(name);
+         int length=name.length();
+         System.out.println("This paragraph contain "+length+"of words");
+         
+        
+     }
+}
