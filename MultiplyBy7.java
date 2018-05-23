286354fba20cbadd8d7cf09c600c2952360a0541
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-05-23 16:00:37 +0530

    Create MultiplyBy7.java

---
+++ MultiplyBy7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class MultiplyBy7
+{
+    
+    public static void main(String[] args) {
+    	
+    	Scanner input = new Scanner(System.in);
+    	
+    
+    	int number = input.nextInt();
+    	System.out.println("You entered " + number);
+    
+
+if(number%7==0)
+{
+System.out.println("yes");
+}
+else
+
+{
+System.out.println("no");
+}    
+    }
+}
+    
