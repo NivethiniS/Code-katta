8b6ff247c8a9a2f8c245f04e73e4db1ca883947f
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-05-24 14:24:30 +0530

    Update Factor.java

--- Factor.java
+++ Factor.java
@@ -4,13 +4,14 @@ class Main
 public static void main(String arg[])
 {
 Scanner sc=new Scanner(System.in);
-int n=sc.nextInt();
-for(int i=1;i<=n;i++)
-{
-if(n%1==0)
-{
-System.out.println(i);
-}
-}
+
+        int number = sc.nextInt();
+
+        System.out.print("Factors of " + number + " are: ");
+        for(int i = 1; i <= number; ++i) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
 }
 }
