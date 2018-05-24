6101edfd0501a4008b20379a4923be88999fa911
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-05-24 14:29:20 +0530

    Create Multiplyby13.java

---
+++ Multiplyby13.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        if(number%13==0)
+        {
+            
+            System.out.println("this is multiply by 13");
+        }
+        else{
+            
+              System.out.println("no");
+        }
+        
+}
+}
