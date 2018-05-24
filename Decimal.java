71b9a2813fb24f16a068959ae105a2ec642f0e8a
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-05-24 11:19:28 +0530

    Create Decimal.java

---
+++ Decimal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+
+float a=sc.nextFloat(); 
+
+int d = (int) Math.ceil(a);
+
+System.out.println(d);
+}
+}
