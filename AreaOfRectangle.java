26153be149c5ffa98bab5ae8d2f1daa0ad0e12ab
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-05-25 11:41:14 +0530

    Create AreaOfRectangle.java

---
+++ AreaOfRectangle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+float n1=sc.nextFloat();
+float n2=sc.nextFloat();
+float area=n1+n2;
+System.out.println(area);
+}
+}
