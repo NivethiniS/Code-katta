836af1afe37a3c934a3d9892c161c4e9332ce741
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-05-25 14:49:34 +0530

    Create Cuboid.java

---
+++ Cuboid.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+float n1=sc.nextFloat();
+float n2=sc.nextFloat();
+float n3=sc.nextFloat();
+float n=n1*n2*n3;
+System.out.println(n);
+}
+}
