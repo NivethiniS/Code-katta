559b420a6580d3a913f3553bbe7b17205b997ef9
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-05-25 12:03:33 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+float n1=sc.nextFloat();
+float n2=sc.nextFloat();
+float m=n1%n2;
+float d=n1/n2;
+System.out.println(m);
+System.out.println(m);
+}
+}
